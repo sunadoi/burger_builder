8640ee4fc846a6fc2208721133c7703995762517
sunadoi <y.sunadoi@gmail.com>
2020-04-25 08:48:48 +0900

    Create Layout component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello, world</h1>
-      </header>
+    <div>
+      <Layout>
+        <p>hello, world</p>
+      </Layout>
     </div>
   );
 }
