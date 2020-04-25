b2e4ab96a2d060592d2a04b39464079243879943
sunadoi <y.sunadoi@gmail.com>
2020-04-25 14:47:59 +0900

    modify BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuilderControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...}
+  // }
+
+  state = {
+    ingredients: {
+      salad: 2,
+      bacon: 1,
+      cheese: 2,
+      meat: 1
+    }
+  }
+
+
   render () {
     return (
       <Aux>
-        <Burger />
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuilderControls />
       </Aux>
     )
   }
