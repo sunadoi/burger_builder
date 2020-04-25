325929f2d09f2b02a1eb209ad19070ee79ed1492
sunadoi <y.sunadoi@gmail.com>
2020-04-25 10:50:30 +0900

    render BurgerIngredient

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Burger from './components/Burger/Burger';
 import './App.module.css';
 
 function App() {
