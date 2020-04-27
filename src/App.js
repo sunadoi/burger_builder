d51eff790d933fcc582685cae7b00c8f377fd597
sunadoi <y.sunadoi@gmail.com>
2020-04-28 08:09:48 +0900

    create checkout summary

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import './App.module.css';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render () {
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+
+import Aux from '../Aux/Aux';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+    state = {
+      error: null
+    }
+
+    componentDidMount () {
+      this.reqInterceptor = axios.interceptors.request.use(request => {
+        this.setState({error: null});
+        return request;
+      });
+      this.resInterceptor = axios.interceptors.response.use(request => request, error => {
+        this.setState({error: error});
+      })
+    }
+
+    componentWillUnmount () {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({error: null});
+    }
+
+    render () {
+      return (
+        <Aux>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      )
+    }
+  }
+}
+
+export default withErrorHandler;
