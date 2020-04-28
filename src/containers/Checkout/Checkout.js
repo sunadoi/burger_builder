e936ba1bb347912186dd04fdb6e5bc9ca6f1244d
sunadoi <y.sunadoi@gmail.com>
2020-04-28 20:24:30 +0900

    contactData

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
@@ -33,10 +35,13 @@ class Checkout extends Component {
   render () {
     return (
       <div>
-        <CheckoutSummary 
+        <CheckoutSummary
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler} />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData} />
       </div>
     )
   }
