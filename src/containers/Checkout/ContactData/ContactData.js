4a5a159cea286385c91be59bfc8eae69a70f7b80
sunadoi <y.sunadoi@gmail.com>
2020-04-28 22:32:32 +0900

    Order

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.module.css';
+
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import axios from '../../../axios-orders';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: ''
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({loading: true})
+
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'Suna',
+        address: {
+          street: 'street1',
+          postalCode: '123',
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => {
+        console.log(response);
+        this.setState({loading: false})
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({loading: false})
+      })
+  }
+
+  render () {
+    let form = (
+      <form>
+        <input className={classes.Input} type="text" name="name" placeholder="Your name" />
+        <input className={classes.Input} type="email" name="email" placeholder="Your email" />
+        <input className={classes.Input} type="text" name="street" placeholder="Your street" />
+        <input className={classes.Input} type="text" name="postal" placeholder="Your postal" />
+        <Button
+          btnType="Success"
+          clicked={this.orderHandler}>ORDER</Button>
+      </form>);
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Data</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default ContactData;
