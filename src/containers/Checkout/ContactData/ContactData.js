d0f5668238ebb609ed98308bfcd707149e143ae3
sunadoi <y.sunadoi@gmail.com>
2020-04-29 11:02:48 +0900

    dropdown validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -81,7 +81,8 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'cheapest'},
           ]
         },
-        value: ''
+        value: '',
+        valid: true
       }
     },
     loading: false
@@ -162,6 +163,8 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success">ORDER</Button>
