99d3f9c2cad93120deacb599e3929f0b99d0aa68
sunadoi <y.sunadoi@gmail.com>
2020-04-30 22:20:03 +0900

    ajust checkout and contactData

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -44,8 +44,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    
-    this.setState({purchasable: sum > 0})
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -57,16 +56,9 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    const queryParams = [];
-    for (let i in this.state.ingredients) {
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-    }
-    queryParams.push('price=' + encodeURIComponent(this.state.totalPrice))
-    const queryString = queryParams.join('&');
-    this.props.history.push({
-      pathname: '/checkout',
-      search: '?' + queryString
-    });
+    this.props.history.push{
+      this.props.history.push('/checkout');
+    };
   }
 
   render () {
@@ -88,7 +80,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             price={this.props.price}
             ordered={this.purchaseHandler}
           />
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,8 @@ import axios from '../../../axios-orders';
 
 import Input from '../../../components/UI/Input/Input';
 
+import { connect } from 'react-redux';
+
 class ContactData extends Component {
   state = {
     orderForm: {
@@ -106,7 +108,7 @@ class ContactData extends Component {
     }
 
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData
     }
@@ -195,4 +197,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  }
+}
+
+export default connect(mapStateToProps)(ContactData);
