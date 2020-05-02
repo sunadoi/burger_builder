ddcd056d5c7fa72570add42c4a13d571d391417e
sunadoi <y.sunadoi@gmail.com>
2020-05-02 11:33:18 +0900

    refactoring by utility.js

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   orders: [],
@@ -10,49 +11,27 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.PURCHASE_BURGER_INIT:
-      return {
-        ...state,
-        purchased: false
-      }
-
+      return updateObject(state, { purchased: false })
     case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: true
-      }
+      return updateObject(state, { loading: true })
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId
-      }
-      return {
-        ...state,
+      const newOrder = updateObject(action.orderData, { id: action.orderId })
+      return updateObject(state, {
         loading: false,
         orders: state.orders.concat(newOrder),
         purchased: true
-      }
+      })
     case actionTypes.PURCHASE_BURGER_FAIL:
-      return {
-        ...state,
-        loading: false
-      }
+      return updateObject(state, { loading: false })
     case actionTypes.FETCH_ORDERS_START:
-      return {
-        ...state,
-        loading: true,
-        error: false
-      }
+      return updateObject(state, { loading: true })
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return {
-        ...state,
+      return updateObject(state, {
         orders: action.fetchedOrders,
         loading: false
-      }
+      })
     case actionTypes.FETCH_ORDERS_FAIL:
-      return {
-        ...state,
-        loading: false
-      }
+      return updateObject(state, { loading: false })
     default:
       return state;
   }
