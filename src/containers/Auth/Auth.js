2bf09209ef78c642f58f96654852a3e16f29285d
sunadoi <y.sunadoi@gmail.com>
2020-05-03 09:29:24 +0900

    create Auth and axios

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+import classes from './Auth.module.css';
+
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
+
+class Auth extends Component {
+
+  state = {
+    controls: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Mail Address'
+        },
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Password'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false,
+      },
+    },
+    isSignup: true
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        touched: true
+      }
+    }
+    this.setState({controls: updatedControls});
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {isSignup: !prevState.isSignup}
+    })
+  }
+
+  render() {
+    const formElementArray = [];
+    for (let key in this.state.controls) {
+      formElementArray.push({
+        id: key,
+        config: this.state.controls[key]
+      })
+    }
+
+    let form = <Spinner />
+    if (!this.props.loading) {
+      form = formElementArray.map(formElement => (
+        <Input
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+          invalid={!formElement.config.valid}
+          shouldValidate={formElement.config.validation}
+          touched={formElement.config.touched}
+          changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+      ))
+    }
+
+    let errorMessage = null;
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      )
+    }
+
+    return (
+      <div className={classes.Auth}>
+        <form onSubmit={this.submitHandler}>
+          {errorMessage}
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
+        <Button
+          clicked={this.switchAuthModeHandler}
+          btnType="Danger">SWITCH TO {this.state.isSignup ? "SIGNIN" : "SIGNUP"}</Button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    error: state.auth.error,
+    loading: state.auth.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
