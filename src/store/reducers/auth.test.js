f162fac39080c7407f6a3d67fe51ea9b8a6db48e
sunadoi <y.sunadoi@gmail.com>
2020-05-03 21:23:49 +0900

    fix

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -1,6 +1,5 @@
 import reducer from './auth';
 import * as actionTypes from '../actions/actionTypes';
-import { initIngredients } from '../actions';
 
 describe('auth reducer', () => {
   it('should return the initial state', () => {
