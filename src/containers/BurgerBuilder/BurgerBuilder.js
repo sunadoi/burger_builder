57ded0edaeb53108322d0947f57aa3edf00660d6
sunadoi <y.sunadoi@gmail.com>
2020-05-03 21:06:01 +0900

    container test

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
   //   this.state = {...}
