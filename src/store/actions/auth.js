bbc296da90a603ebd1212e710bbe24a205b364af
sunadoi <y.sunadoi@gmail.com>
2020-05-04 13:52:13 +0900

    checkAuthTimeout delay

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -31,12 +31,17 @@ export const logout = () => {
   }
 }
 
-export const checkAuthTimeout = (expirationTime) => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(logout());
-    }, expirationTime * 1000);
+export const logoutSucceed = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
   }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+  return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    expirationTime: expirationTime
+  };
 }
 
 export const auth = (email, password, isSignUp) => {
