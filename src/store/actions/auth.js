5f4ef2eb3ac810d19757c2a70962cf0c35340234
sunadoi <y.sunadoi@gmail.com>
2020-05-04 14:08:55 +0900

    authUserSaga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -45,29 +45,11 @@ export const checkAuthTimeout = (expirationTime) => {
 }
 
 export const auth = (email, password, isSignUp) => {
-  return dispatch => {
-    dispatch(authStart());
-    const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    };
-    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCo9b8gVTzZhEWHMNRMkO9yrAxzapzo1-Y';
-    if (!isSignUp) {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCo9b8gVTzZhEWHMNRMkO9yrAxzapzo1-Y'
-    }
-    axios.post(url, authData)
-      .then(response => {
-        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-        localStorage.setItem('token', response.data.idToken);
-        localStorage.setItem('userId', response.data.localId);
-        localStorage.setItem('expirationDate', expirationDate);
-        dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.expiresIn));
-      })
-      .catch(error => {
-        dispatch(authFail(error.response.data.error));
-      })
+  return {
+    type: actionTypes.AUTH_USER,
+    email: email,
+    password: password,
+    isSignUp: isSignUp
   }
 }
 
