cc0aad98873c1cdb611563cdee91865062529120
sunadoi <y.sunadoi@gmail.com>
2020-05-05 16:08:56 +0900

    solve dependencies

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,10 +14,11 @@ import axios from '../../axios-orders';
 
 export const BurgerBuilder = props => {
   const [purchasing, setPurchasing] = useState(false);
+  const { onInitIngredients } = props;
 
   useEffect(() => {
-    props.onInitIngredients();
-  }, [])
+    onInitIngredients();
+  }, [onInitIngredients])
 
   const updatePurchaseState = (ingredients) => {
     const sum = Object.keys(ingredients)
