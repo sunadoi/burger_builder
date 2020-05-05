d5909cd8199e53022a116d4e51c1e13fb4f00706
sunadoi <y.sunadoi@gmail.com>
2020-05-05 15:35:23 +0900

    various class to hooks

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 // import PropType from 'prop-types';
 
-class OrderSummary extends Component {
+const OrderSummary = props => {
   //This could be a functional component, doesn't have to be a class.
 
-  render () {
-    const ingredientSummary = Object.keys(this.props.ingredients)
-      .map(igKey => {
-        return (
-          <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}: {this.props.ingredients[igKey]}</span>
-          </li>
-        )
-      })
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}: {props.ingredients[igKey]}</span>
+        </li>
+      )
+    })
 
-    return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
-        <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
-      </Aux>
-    )
-  }
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
+      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+    </Aux>
+  )
 } 
 
 
